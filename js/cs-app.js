d3f68fc9eead7adf90aacdeff49b9d7d6b84242b
Grant Carthew <grant@carthew.net>
2016-11-27 23:00:35 +1000

    phonebook added

--- js/cs-app.js
+++ js/cs-app.js
@@ -9,18 +9,29 @@ var app = new Vue({
       citrix: {},
       vmware: {},
       itil: {}
+    },
+    phonebook: {
+
     }
   },
   created: function () {
-    this.getResources().then(function () {
+    return Promise.all([
+      this.getResources(),
+      this.getPhonebook()
+    ]).then(function () {
       $(document).foundation()
     }).catch(function (err) { console.error(err) })
   },
   methods: {
     getResources: function () {
       return this.$http.get('data/resources.json').then(function (res) {
-        console.log('Body: ', res.body)
+        console.log('Resources Body: ', res.body)
         this.resources = res.body
+      })
+    },
+    getPhonebook: function () {
+      return this.$http.get('data/phonebook.xml').then(function (book) {
+        console.log('Phonebook Body: ', book.body)
       })
     }
   }
