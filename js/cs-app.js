5ad464e883dbcaf1cd4f53b1dcd55fb5f286aa41
Grant Carthew <grant@carthew.net>
2016-11-28 14:51:13 +1000

    Added TP tab

--- js/cs-app.js
+++ js/cs-app.js
@@ -1,38 +1,57 @@
 var app = new Vue({
   el: '#app',
   data: {
-    resources: {
-      common: {},
-      whs: {},
-      microsoft: {},
-      cisco: {},
-      citrix: {},
-      vmware: {},
-      itil: {}
-    },
-    phonebook: {
-
-    }
+    loaded: false,
+    resources: {},
+    phonebook: {}
   },
   created: function () {
+    var self = this
     return Promise.all([
       this.getResources(),
       this.getPhonebook()
     ]).then(function () {
-      $(document).foundation()
+      self.loaded = true
     }).catch(function (err) { console.error(err) })
+  },
+  updated: function () {
+    $(document).foundation()
   },
   methods: {
     getResources: function () {
       return this.$http.get('data/resources.json').then(function (res) {
-        console.log('Resources Body: ', res.body)
         this.resources = res.body
       })
     },
     getPhonebook: function () {
-      return this.$http.get('data/phonebook.xml').then(function (book) {
-        console.log('Phonebook Body: ', book.body)
+
+      var self = this
+      return this.$http.get('data/phonebook.xml', {
+        headers: {'Content-Type': 'text/xml'}
+      }).then(function (book) {
+        return self.convertBlobToXml(book.body)
+      }).then(function (xml) {
+        var x2js = new X2JS()
+        var contacts = x2js.dom2js(xml).LocalPhoneBook.Contact
+        contacts = contacts.sort(self.compareDisplayName)
+        self.phonebook = contacts
       })
+    },
+    convertBlobToXml: function (blob) {
+      return new Promise(function (resolve, reject) {
+        var reader = new FileReader()
+        reader.addEventListener('loadend', function () {
+          var xml = $.parseXML(reader.result)
+          resolve(xml)
+        })
+        reader.readAsText(blob)
+      })
+    },
+    compareDisplayName: function (a, b) {
+      var dnA = a.DisplayName
+      var dnB = b.DisplayName
+      return dnA < dnB ? -1 : 1
     }
+
   }
 })
