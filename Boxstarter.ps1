c6978875a49e0e311664e20f0209253e0a8c99b3
Grant Carthew <grant@carthew.net>
2017-10-03 12:52:28 +1000

    Update Boxstarter.ps1

--- Boxstarter.ps1
+++ Boxstarter.ps1
@@ -16,5 +16,4 @@ Install-WindowsFeature -Name Telnet-Client,Xps-Viewer
 Set-Service -Name 'WSearch' -StartupType Automatic
 Start-Service -Name 'WSearch'
 
-Install-WindowsUpdate -AcceptEula -Criteria "BrowseOnly=0 OR BrowseOnly=1 AND IsAssigned=1 AND IsHidden=0 AND IsInstalled=0 AND Type='Software' OR Type='Driver'"
 Rename-Computer -NewName Base2016Audit -Restart -Force
